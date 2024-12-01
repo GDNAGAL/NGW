743d3c66a02768792d5de734a9546223bdc69c25
GD NAGAL <ganpatram@GORDHANs-MacBook-Air.local>
2024-12-02 00:43:15 +0530

    updates

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -45,21 +45,21 @@ const HomePage = () => {
           </div>
           <div className="carousel-inner">
             <div className="carousel-item active">
-              <img src="/Images/A1.JPG" className="d-block w-100" alt="..."/>
+              <img src="Images/A1R.png" className="d-block w-100" alt="..."/>
               <div className="carousel-caption d-none d-md-block">
                 <h5>First slide label</h5>
                 <p>Some representative placeholder content for the first slide.</p>
               </div>
             </div>
             <div className="carousel-item">
-              <img src="/Images/A1.JPG" className="d-block w-100" alt="..."/>
+              <img src="Images/A1R.png" className="d-block w-100" alt="..."/>
               <div className="carousel-caption d-none d-md-block">
                 <h5>Second slide label</h5>
                 <p>Some representative placeholder content for the second slide.</p>
               </div>
             </div>
             <div className="carousel-item">
-              <img src="/Images/A1.JPG" className="d-block w-100" alt="..."/>
+              <img src="Images/A1R.png" className="d-block w-100" alt="..."/>
               <div className="carousel-caption d-none d-md-block">
                 <h5>Third slide label</h5>
                 <p>Some representative placeholder content for the third slide.</p>
