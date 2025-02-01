cd9447970aec2d9d61d28b0ad157e14cb6c0cf81
GDNAGAL <128776145+GDNAGAL@users.noreply.github.com>
2025-02-01 10:52:19 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import sitemap from "vite-plugin-sitemap";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   publicDir: 'public',
-  plugins: [react()],
+  plugins: [
+    react(),
+    sitemap({
+      hostname: "https://narainglobalacademy.com",
+      dynamicRoutes: [
+        "/about",
+        "/why-choose-us",
+        "/classroom-facilities",
+        "/hostel-facilities",
+        "/safety-security",
+        "/daily-schedule",
+      ], // Add all important pages
+    })
+  ],
   // server: {
   //   host: '192.168.64.123', 
   //   port: 5200, 
